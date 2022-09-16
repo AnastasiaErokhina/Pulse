b0a6ed7951e5e6fa99b36d9d226818d066ec6e1c
Erokhina Anastasia <eroxina1997@bk.ru>
2022-09-16 16:49:35 +0700

    creation of the catalog section (tabs and product cards) and adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,46 @@
-const slider = tns({
-    container: '.slider-tns__inner',
-    items: 1,
-	autoHeight: true,
-	controls: false,
-	navPosition: 'bottom',
-	speed: 1500,
-	autoplay: true,
-	autoplayButtonOutput: false,
-	autoplayTimeout: 3000,
-    responsive: {
-		992: {
-			nav: false
-		  },
-		320: {
-			nav: true
+$(document).ready(function() {
+	const slider = tns({
+		container: '.slider-tns__inner',
+		items: 1,
+		autoHeight: false,
+		controls: false,
+		navPosition: 'bottom',
+		speed: 1500,
+		autoplay: true,
+		autoplayButtonOutput: false,
+		autoplayTimeout: 3000,
+		responsive: {
+			992: {
+				nav: false
+			},
+			320: {
+				nav: true
+			}
 		}
-    }
-  });
-document.querySelector('.slider-tns__prev').addEventListener('click', function () {
-	slider.goTo('prev');
-});
-document.querySelector('.slider-tns__next').addEventListener('click', function () {
-	slider.goTo('next');
+	});
+	document.querySelector('.slider-tns__prev').addEventListener('click', function () {
+		slider.goTo('prev');
+	});
+	document.querySelector('.slider-tns__next').addEventListener('click', function () {
+		slider.goTo('next');
+	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.cards-product').removeClass('cards-product_active').eq($(this).index()).addClass('cards-product_active');
+	  });
+
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.card-product__front-side').eq(i).toggleClass('card-product__front-side_active');
+				$('.card-product__flip-side').eq(i).toggleClass('card-product__flip-side_active');
+			})
+		});
+	};
+
+	toggleSlide('.card-product__link');
+	toggleSlide('.card-product__link-back');
 });
